00b2302307555911b586e0d329b2d2e53e0a907d
bjspencer <brandonspen@gmail.com>
2018-07-24 17:54:02 -0400

    removed unused variable

--- src/ValidatingInput.java
+++ src/ValidatingInput.java
@@ -9,23 +9,22 @@ public class ValidatingInput {
 		String userEmail;
 		String userPhoneNum;
 		String userDate;
-		boolean prompt;
-		
+				
 		System.out.println("Please enter a valid name: ");
 		userName = scan.nextLine();
-		prompt = isName(userName);
+		isName(userName);
 		
 		System.out.println("Please enter a valid email address: ");
 		userEmail = scan.nextLine();
-		prompt = isEmail(userEmail);
+		isEmail(userEmail);
 		
 		System.out.println("Please enter a valid phone number: ");
 		userPhoneNum = scan.nextLine();
-		prompt = isPhone(userPhoneNum);
+		isPhone(userPhoneNum);
 		
 		System.out.println("Please enter a valid date: ");
 		userDate = scan.nextLine();
-		prompt = isDate(userDate);
+		isDate(userDate);
 		
 		
 		scan.close();
@@ -42,7 +41,7 @@ public class ValidatingInput {
 	   }
 	  
 	   public static boolean isEmail(String s) {
-	       if(Pattern.matches("[A-Za-z0-9]{5,30}@[A-Za-z0-9]{5,10}.[A-Za-z0-9]{2,3}",s)) {
+	       if(Pattern.matches("[A-Za-z0-9]{5,30}\\w+@[A-Za-z0-9]{5,10}\\.[A-Za-z]{2,3}",s)) {
 	          System.out.println("That is a valid email address!");
 	    	   return true;
 	       }
@@ -61,7 +60,7 @@ public class ValidatingInput {
 	   }
 	  
 	   public static boolean isDate(String s) {
-	       if(Pattern.matches("(0?[1-9]|[12][0-9]|3[01])/(0?[1-9]|1[012])/([0-9]{4})/",s)) {
+	       if(Pattern.matches("\\d{2}.\\d{2} .\\d{4}",s)) {
 	          System.out.println("That is a valid date!");
 	    	   return true;
 	       }
